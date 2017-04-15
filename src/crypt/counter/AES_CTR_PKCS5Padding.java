0635000bd99f05ee9504b3892bae677f1c84c810
Dzulham Azham <dzulham.azham@gmail.com>
2017-04-15 17:21:10 +0700

    Change AES_CTR_PKCS5Padding to be static class

--- src/crypt/counter/AES_CTR_PKCS5Padding.java
+++ src/crypt/counter/AES_CTR_PKCS5Padding.java
@@ -8,51 +8,43 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-public final class AES_CTR_PKCS5Padding {
-
+public final class AES_CTR_PKCS5Padding 
+{
     public static final String CIPHER_MODE = "AES/CTR/PKCS5PADDING";
+    public static Cipher CIPHER = null;
     public static byte[] IV;
-    private final Cipher cipher;
 
-    public AES_CTR_PKCS5Padding() throws NoSuchAlgorithmException, NoSuchPaddingException 
+    public static void init() throws NoSuchAlgorithmException, NoSuchPaddingException 
     {
-        cipher = Cipher.getInstance(AES_CTR_PKCS5Padding.CIPHER_MODE);
+        CIPHER = Cipher.getInstance(AES_CTR_PKCS5Padding.CIPHER_MODE);
         SecureRandom srandom = SecureRandom.getInstance("SHA1PRNG");
-        AES_CTR_PKCS5Padding.IV = new byte[cipher.getBlockSize()];
+        AES_CTR_PKCS5Padding.IV = new byte[CIPHER.getBlockSize()];
         srandom.nextBytes(AES_CTR_PKCS5Padding.IV);
     }
     
-    public String encrypt(String key, String value) {
-        try {
-            IvParameterSpec iv = new IvParameterSpec(AES_CTR_PKCS5Padding.IV);
-            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
-
-            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
-
-            byte[] encrypted = cipher.doFinal(value.getBytes());
-            
-            return Base64.getEncoder().encodeToString(encrypted);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return null;
+    private static void assertCIPHER() throws NoSuchAlgorithmException, NoSuchPaddingException 
+    {
+        if(CIPHER == null)
+            AES_CTR_PKCS5Padding.init();
+    }
+    
+    public static String encrypt(String key, String value) throws Exception
+    {
+        assertCIPHER();
+        IvParameterSpec iv = new IvParameterSpec(AES_CTR_PKCS5Padding.IV);
+        SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+        CIPHER.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+        byte[] encrypted = CIPHER.doFinal(value.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
     }
 
-    public String decrypt(String key, String encrypted) {
-        try {
-            IvParameterSpec iv = new IvParameterSpec(AES_CTR_PKCS5Padding.IV);
-            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
-
-            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-
-            byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
-
-            return new String(original);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return null;
+    public static String decrypt(String key, String encrypted) throws Exception
+    {
+        assertCIPHER();
+        IvParameterSpec iv = new IvParameterSpec(AES_CTR_PKCS5Padding.IV);
+        SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+        CIPHER.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+        byte[] original = CIPHER.doFinal(Base64.getDecoder().decode(encrypted));
+        return new String(original);
     }
 }
